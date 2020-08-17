86542d925aa48b7c832e8c0ed4329b4335ab9648
Theosantini <theosantunes@gmail.com>
2020-08-17 19:10:54 -0300

    new commit2

--- D. aggregation IPEA.py
+++ D. aggregation IPEA.py
@@ -15,7 +15,7 @@ dataframesn = [pd.read_excel(f) for f in filenamesn]
 
 dadosn = pd.concat(dataframesn, ignore_index=True)
 
-# Manipular e organizar matriz insumo produto 2000-2017 para obter apenas variáveis essenciais
+# Filtrar e organizar matriz insumo produto 2000-2017 para obter apenas variáveis essenciais
 
 var = ['CI', 'Remunerações', 'Valor adicionado bruto ( PIB )','Valor da produção', 'Fator trabalho (ocupações)']
 
@@ -27,7 +27,9 @@ dadosn = (dadosn[dadosn.Variáveis.isin(var)]
                .rename(columns = {'CI' : 'vQji', 'Fator trabalho (ocupações)' : 'rLi',
                       'Remunerações' : 'vLi', 'Valor adicionado bruto ( PIB )' : 'vVA',
                       'Valor da produção' : 'vQi'})
-               .assign(vKi = lambda x: x.vVA - x.vLi))
+               .assign(vKi = lambda x: x.vVA - x.vLi)
+               .assign(nKi = lambda x: x.vVA - x.vKi +x.vLi)) # estoque de capital (fictício)
+
 
 ### Importar matrizes de insumo-produto valores constantes
 
@@ -39,7 +41,7 @@ dataframesr = [pd.read_excel(f) for f in filenamesr]
 
 dadosr = pd.concat(dataframesr, ignore_index=True)
 
-# Manipular e organizar matriz insumo produto 2000-2017 para obter apenas variáveis essenciais
+# Filtrar e organizar matriz insumo produto 2000-2017 para obter apenas variáveis essenciais
 
 var = ['CI', 'Valor da produção']
 
@@ -53,12 +55,52 @@ dadosr = (dadosr[dadosr.Variáveis.isin(var)]
 
 ### Juntar dadosn e dadosr no mesmo dataframe
 
-dados = pd.concat([dadosn,dadosr], ignore_index=False, axis=1)
+dados = pd.merge(dadosn,dadosr, on=['Ano','Setores'])
 
-# Agregar setores em 10
+# Agregar setores em 10 ou 12
 
-# Adicionar inflação e calcular variação real do estoque de capital
+# Adicionar inflação (IGP-DI) e calcular variação real do estoque de capital
 
-# Calcular PIB nominal anuall
+Infla = [9.52, 10.23, 27.66, 6.95, 11.87, 1.42, 3.64, 8.19,
+         8.57, -0.94, 11.28, 4.64, 8.10, 5.57, 3.92, 11.17, 6.6, -0.35]
 
+Ano = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
+        2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]
+
+Infla = pd.DataFrame({'Ano': Ano, 'Infla': Infla})
+
+Infla['Infla'] = Infla['Infla'] / 100
+
+dados = pd.merge(dados, Infla, on='Ano')
+
+# Calcular PIB nominal anual
+
+dados['PIB'] = dados.groupby('Ano')['vVA'].transform(sum)
+
+# Funções necessárias para calcular produtividade (MFP) setorial
+#([1] shares nominais e [2] taxas de crescimento)
+
+# [1] shares nominais
+
+def sn(x):
+    c = []
+    for i in range(len(x)):
+        if i == 0:
+            c.append(0)
+        else:
+            c.append(0.5 * (x.values[i] + x.values[i - 1]))
+    return c
+
+# [2] taxas de crescimento
+
+def gr(x):
+    c = []
+    for i in range(len(x)):
+        if i == 0:
+            c.append(0)
+        else:
+            c.append(np.log(x.values[i]) - np.log(x.values[i - 1]))
+    return c
+
+# Aplicar funções para produzir variáveis necessárias
 
